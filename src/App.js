4e3ebc9ddcff3c1e769682868e3edbb0fd08af4a
okkyprasetian <okkyprasetian@gmail.com>
2021-12-01 20:43:17 +0700

    finish project

--- src/App.js
+++ src/App.js
@@ -5,15 +5,60 @@ function App() {
 
   // State
   const [list, setList] = useState([])
+  const [item, setItem] = useState('')
+  const [edit, setEdit] = useState(false)
+  const [editIndex, setEditIndex] = useState(null)
+
+  // Set State
+  const handleSubmit = e => {
+
+    e.preventDefault()
+    if (!edit) {
+      setList([...list, item])
+      setItem('')
+    } else {
+      const newList = [...list]
+      newList[editIndex] = item
+      setList(newList)
+      setEdit(false)
+      setItem('')
+    }
+  }
+  const clearList = () => {
+    setList([])
+    setEdit(false)
+    setItem('')
+  }
+  const deleteItem = index => {
+    const newList = [...list]
+    newList.splice(index, 1)
+    console.log(index)
+    setList(newList)
+  }
+  const activateEdit = index => {
+    setEdit(true)
+    setEditIndex(index)
+    setItem(list[index])
+  }
 
   return (
     <div className="App">
       <main className="main">
         <h1 className="header">Grocery Bud</h1>
-        <form className="form">
-          <input type="text" className="input" placeholder="e.g. chicken" />
-          <button className="button" type="submit">Submit</button>
+        <form className="form" onSubmit={handleSubmit}>
+          <input type="text" className="input" value={item} placeholder="e.g. chicken" onChange={e => setItem(e.target.value)} />
+          <button className="submit" type="submit"> {edit ? 'Edit' : 'Submit'} </button>
         </form>
+        {list.length > 0 &&
+          <article className="lists">
+            {list.map((e, index) => (<List e={e} index={index} deleteItem={deleteItem} activateEdit={activateEdit} key={index} />))}
+          </article>
+        }
+        {list.length > 0 &&
+          <div className="footer">
+            <div className="clear hover" onClick={clearList}>Clear Items</div>
+          </div>
+        }
       </main>
     </div>
   );
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,14 @@
-function List() {
+import { MdEdit } from 'react-icons/md'
+import { FaTrash } from 'react-icons/fa'
+
+function List({ e, index, deleteItem, activateEdit }) {
     return (
         <div className="list">
-            list
+            <p className="list-left">{e}</p>
+            <p className="list-right">
+                <MdEdit className="edit hover" onClick={() => activateEdit(index)} />
+                <FaTrash className="delete hover" onClick={() => deleteItem(index)} />
+            </p>
         </div>
     );
 }
